606ca92067908793dc5db0a4c9c948f6f21bc482
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-04-21 08:31:29 -0300

    :lipstick: Change TheHeader structure

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,13 +1,13 @@
 <template>
-  <nav>
-      <img src="../assets/logo.svg" alt="">
-      <ul>
+  <nav class="navigation">
+      <img class="navigation--image" src="../assets/logo.svg" alt="">
+      <ul class="navigation--links">
         <li>Início</li>
         <li>Preços</li>
         <li>Sobre nós</li>
       </ul>
 
-      <div>
+      <div class="navigation--buttons">
         <button>Login</button>
         <button>Assine</button>
       </div>
@@ -17,5 +17,34 @@
 <script>
 </script>
 
-<style>
+<style lang="scss" scoped>
+.navigation {
+  width: 100vw;
+  height: 5em;
+  padding: 2em;
+  border: 1px solid red;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .navigation--image {
+    width: 3em;
+    height: 3em;
+  }
+
+  .navigation--links {
+    display: flex;
+    list-style: none;
+
+    li {
+      padding: 0 1em;
+    }
+  }
+
+  .navigation--buttons{
+    align-self: flex-end;
+    justify-self: flex-end;
+  }
+}
 </style>
